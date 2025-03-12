0f530b186c3636d7301863912a7135f52ceaab1c
johncarlo-dizon <johncarlovdizon@gmail.com>
2025-03-12 22:49:34 +0800

    variables

---
+++ lib/variable.dart
@@ -0,0 +1,16 @@
+import 'dart:ffi';
+
+import 'package:ashborn/main.dart';
+import 'package:flutter/cupertino.dart';
+
+bool navmode = true;
+bool gesmode = false;
+
+String quotefontFamily = "Georgia";
+String titlefontFamily = "Georgia";
+bool quotefamily = true;
+String quoteSet = "QUOTE/VERSE";
+
+double quotefontSize = 18.0;
+double titlefontSize = 15.0;
+Widget lastPage = MyApp();
